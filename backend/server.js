efaf6a93b18f03dfbf98ac0eee859ebd895435b4
RohitKumar186 <rohitkumar186singh@gmail.com>
2026-08-29 00:52:59 +0530

    Test ESP32 route

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import "./src/db/pool.js";
+import esp32Router from "./src/routes/esp32.js";
 
 import monitoringRouter from "./src/routes/monitoring.js";
 import historyRouter from "./src/routes/history.js";
 import predictionRouter from "./src/routes/prediction.js";
 import inventoryRouter from "./src/routes/inventory.js";
+
 //import "./src/dummySensor.js";
 
 dotenv.config();
@@ -22,10 +24,21 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
+app.get("/api/esp32-test", (req, res) => {
+  res.json({
+    status: "ok",
+    route: "esp32 route loaded"
+  });
+});
+
+
+
 app.use("/api/monitoring", monitoringRouter);
 app.use("/api/history", historyRouter);
 app.use("/api/prediction", predictionRouter);
 app.use("/api/inventory", inventoryRouter);
+
+app.use("/api/esp32", esp32Router);
 
 app.use((req, res) => {
   res.status(404).json({ error: "Not found" });
